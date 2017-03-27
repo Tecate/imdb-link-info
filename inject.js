88a5d415bc2753a40bc7de2df39fb4b739d15e74
Tec8 <kawnsull@gmail.com>
2017-03-27 13:33:00 -0400

    Cleaned up functions

--- inject.js
+++ inject.js
@@ -1,16 +1,28 @@
+// Colors
+var colorBg = "#f8f8f8";
+var colorBorder = "#a1a1a1";
+var colorRed = "#F7977A";
+var colorYellow = "#FFF79A"
+var colorGreen = "#82CA9D";
+
+function scoreColor(score) {
+	var scoreColor;
+	if (score.indexOf(".")!=-1) // check if score is imdb rating
+		score = score * 10; // convert to metascore
+	if (score <= 39) { scoreColor = colorRed; };
+	if (score > 39 && score <= 60) { scoreColor = colorYellow; };
+	if (score > 60) { scoreColor = colorGreen; };
+	return scoreColor;
+}
+
 (function() {
-
-	function metascoreColor(metascore) {
-		if (metascore <= 39) { return "red"; }
-		if (metascore > 39 && metascore <= 60) { return "yellow"; }
-		if (metascore > 60) { return "green"; }
-	}
-
 	$('a[href*="imdb.com"]').each(function() { // Every link that contains "imdb.com"
 		var imdbLink = $(this); // The actual link element
 		var imdbUrl = $(this).attr("href"); // The URL
 		var imdbID = imdbUrl.match("tt\\d{7}"); // The imdb id
 		// imdbID = imdbID[0].replace(/\//g, ""); // Trimming slashes
+
+		chrome.extension.getURL("images/imdb.png");
 
 	  var sUrl = 'https://www.omdbapi.com/?i=' + imdbID + '&type=movie&tomatoes=true'
 		$.ajax(sUrl, {
@@ -19,12 +31,12 @@
 	        $('<span />', {
 		        class: "imdb-info",
 		        html:  '('+oData.Year+')' + 
-		        			' ['+oData.imdbRating+']' +
-		        			' [<span style="color: '+metascoreColor(oData.Metascore)+'">'+oData.Metascore+'</span>]' +
-		        			' {'+oData.Genre+'}'
+		        			' <img style="vertical-align: middle;" src="'+chrome.extension.getURL("img/imdb-small.png")+'"> <span style="background: '+scoreColor(oData.imdbRating)+'">'+oData.imdbRating+'</span>' +
+		        			' <img style="vertical-align: middle;" src="'+chrome.extension.getURL("img/metacritic-small.png")+'"> <span style="background: '+scoreColor(oData.Metascore)+'">'+oData.Metascore+'</span>' +
+		        			' '+oData.Genre+''
 		      }).css({
-		      	"background": "#f8f8f8",
-		      	"border": "1px solid #a1a1a1",
+		      	"background": colorBg,
+		      	"border": "1px solid " + colorBorder,
 		      	"border-radius": "1px",
 		      	"margin": "0px 5px"
 		      }).insertAfter(imdbLink);
